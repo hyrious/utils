4e6b84235ca07425f7859ddc63d6670fa5e75a74
hyrious <hyrious@outlook.com>
2022-08-12 16:57:36 +0800

    add hash

--- src/string.ts
+++ src/string.ts
@@ -7,3 +7,17 @@ const encoder = /* @__PURE__ */ new TextEncoder();
 export function encode(string: string) {
   return encoder.encode(string);
 }
+
+/**
+ * ```js
+ * hash('') // 0
+ * hash('a') // 97
+ * hash('ab') // 3135
+ * ```
+ */
+export function hash(s: string): number {
+  for (var i = s.length - 1, h = 0; i >= 0; --i) {
+    h = (Math.imul(31, h) + s.charCodeAt(i)) | 0;
+  }
+  return h;
+}
